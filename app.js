ddb521a5f34678d051aa7033044f3d0fba47c1ef
osmanforhad <developer.osmanforhad@gmail.com>
2026-01-13 21:26:17 +0600

    setting header for response

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ const html = fs.readFileSync("./Template/index.html", "utf-8");
 const app = http.createServer((request, response) => {
   let path = request.url;
   if (path === "/" || path.toLocaleLowerCase() === "/home") {
+    response.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-header": "hello world",
+    });
     response.end(html.replace("{{%CONTENT%}}", "You are in Home page"));
   } else if (path.toLocaleLowerCase() === "/about") {
+    response.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-header": "hello world",
+    });
     response.end(html.replace("{{%CONTENT%}}", "You are in About page"));
   } else if (path.toLocaleLowerCase() === "/contact") {
+    response.writeHead(200, {
+      "Content-Type": "text/html",
+      "my-header": "hello world",
+    });
     response.end(html.replace("{{%CONTENT%}}", "You are in Contact page"));
   } else {
+    response.writeHead(404, {
+      "Content-Type": "text/html",
+      "my-header": "hello world",
+    });
     response.end(html.replace("{{%CONTENT%}}", "Error 404: Page not found"));
   }
 });
