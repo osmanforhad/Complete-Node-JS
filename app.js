7e746f74be86a9ef56cc86b1602dc29312037f51
osmanforhad <developer.osmanforhad@gmail.com>
2026-01-14 15:11:27 +0600

    transfrom JSON data into HTML

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const fs = require("fs");
 const http = require("http");
 
 const html = fs.readFileSync("./Template/index.html", "utf-8");
+let products = JSON.parse(fs.readFileSync("./Data/products.json", "utf-8"));
+let productListHtml = fs.readFileSync("./Template/product-list.html", "utf-8");
+
+let productHtmlArray = products.map((prod) => {
+  let output = productListHtml.replace("{{%IMAGE%}}", prod.productImage);
+  output = output.replace("{{%NAME%}}", prod.name);
+  output = output.replace("{{%MODELNAME%}}", prod.modeName);
+  output = output.replace("{{%MODELNO%}}", prod.modelNumber);
+  output = output.replace("{{%SIZE%}}", prod.size);
+  output = output.replace("{{%CAMERA%}}", prod.camera);
+  output = output.replace("{{%PRICE%}}", prod.price);
+  output = output.replace("{{%COLOR%}}", prod.color);
+  return output;
+});
 
 const app = http.createServer((request, response) => {
   let path = request.url;
@@ -23,6 +37,15 @@ const app = http.createServer((request, response) => {
       "my-header": "hello world",
     });
     response.end(html.replace("{{%CONTENT%}}", "You are in Contact page"));
+  } else if (path.toLocaleLowerCase() === "/products") {
+    let productResponseHtml = html.replace(
+      "{{%CONTENT%}}",
+      productHtmlArray.join(",")
+    );
+    response.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    response.end(productResponseHtml);
   } else {
     response.writeHead(404, {
       "Content-Type": "text/html",
