753877ad3cae00b202712562f4b676be2b506f1d
osmanforhad <developer.osmanforhad@gmail.com>
2026-01-14 16:39:50 +0600

    parsing Query String from URL

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 const html = fs.readFileSync("./Template/index.html", "utf-8");
 let products = JSON.parse(fs.readFileSync("./Data/products.json", "utf-8"));
@@ -14,11 +15,12 @@ let productHtmlArray = products.map((prod) => {
   output = output.replace("{{%CAMERA%}}", prod.camera);
   output = output.replace("{{%PRICE%}}", prod.price);
   output = output.replace("{{%COLOR%}}", prod.color);
+  output = output.replace("{{%ID%}}", prod.id);
   return output;
 });
 
 const app = http.createServer((request, response) => {
-  let path = request.url;
+  let { query, pathname: path } = url.parse(request.url, true);
   if (path === "/" || path.toLocaleLowerCase() === "/home") {
     response.writeHead(200, {
       "Content-Type": "text/html",
@@ -38,14 +40,18 @@ const app = http.createServer((request, response) => {
     });
     response.end(html.replace("{{%CONTENT%}}", "You are in Contact page"));
   } else if (path.toLocaleLowerCase() === "/products") {
-    let productResponseHtml = html.replace(
-      "{{%CONTENT%}}",
-      productHtmlArray.join(",")
-    );
-    response.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    response.end(productResponseHtml);
+    if (!query.id) {
+      let productResponseHtml = html.replace(
+        "{{%CONTENT%}}",
+        productHtmlArray.join(",")
+      );
+      response.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      response.end(productResponseHtml);
+    } else {
+      response.end("This is a product with ID = " + query.id);
+    }
   } else {
     response.writeHead(404, {
       "Content-Type": "text/html",
